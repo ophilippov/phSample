ca62b870b92ed9ecaaa9109f42f9697b5ae823fc
Oleh Philippov <oleh.philippov@gmail.com>
2019-02-11 17:09:40 +0200

    4 task is half-finished other is done

---
+++ phSample/FileServices/Obj/ObjFileService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
+
+namespace phSample
+{
+    /// <summary>
+    /// A class that provides basic handling files of the type *.obj
+    /// </summary>
+    public class ObjFileService : IFileService<Model3DGroup>
+    {
+        /// <summary>
+        /// Reads a file
+        /// </summary>
+        /// <param name="filePath">The file path</param>
+        /// <returns>Return the data of the <see cref="Model3DGroup"/> type</returns>
+        public Model3DGroup Open(string filePath)
+        {
+            var objLoader = new ObjReader();
+            return objLoader.Read(filePath);
+        }
+
+        /// <summary>
+        /// Saves a file
+        /// </summary>
+        /// <param name="filePath">The file path</param>
+        /// <param name="data">A data to save</param>
+        public void Save(string filePath, Model3DGroup data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
